0d715a2e3afa6712c37209c264da32109c445d1a
nohdj <nohdj@mobigen.com>
2025-03-03 13:51:13 +0900

    card feign

--- cards/src/main/java/com/eazybytes/cards/feignClient/LoanFeignClient.java
+++ cards/src/main/java/com/eazybytes/cards/feignClient/LoanFeignClient.java
@@ -4,8 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "loans")
+
+@FeignClient(name="TEAM1-LOAN-SERVICE", path="/api")
 public interface LoanFeignClient {
-    @GetMapping(value = "/api/getHostName", consumes = "application/json")
+    @GetMapping(value = "/getHostName", consumes = "application/json")
     public ResponseEntity<String> getLoanHostName();
 }
